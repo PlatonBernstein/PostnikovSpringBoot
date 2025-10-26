13e6be88793d36591fd22fd715fc8a5ce01175c5
PlatonBernstein <platon.bernstein@mail.ru>
2025-10-26 12:59:45 +0300

    pagination test rework

--- src/test/java/com/example/demo/services/impl/MouseServiceImplTest.java
+++ src/test/java/com/example/demo/services/impl/MouseServiceImplTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -116,24 +117,24 @@ public class MouseServiceImplTest {
     и ассёртами их сверять, соответственно пейдж ту пейдж*/
     @SneakyThrows
     @Test
-    public void testBigDataGet() {
+    public void testPagination() {
         int numberOfPages = 100;
         int pageSize = 5;
-        List<List<Mouse>> listOfPages = new ArrayList<>(); // двойная коллекция - слишком жёсткая структура
-        for (int i = 0; i < numberOfPages; i++) {
-            List<Mouse> pageOfMouses =  new ArrayList<>();
-            for (int j = 0; j < pageSize; j++) {
-                Mouse mouseCurr = Mouse.builder().name("Mouse" + (i * pageSize + j)).build();
-                pageOfMouses.add(mouseCurr);
-                mouseController.create(mouseCurr);
-            }
-            listOfPages.add(pageOfMouses);
+        List<Mouse> allOfMouses =  new ArrayList<>();
+        for (int i = 0; i < numberOfPages * pageSize; i++) {
+            Mouse mouseCurr = Mouse.builder().name(UUID.randomUUID().toString()).build();
+            allOfMouses.add(mouseCurr);
+            mouseController.create(mouseCurr);
         }
+        allOfMouses.sort(Comparator.comparing(Mouse::getName));
+
 
         for (int i = 0; i < numberOfPages; i++) {
-            List<Mouse> pageOfCreatedMouses = mouseController.getAll(PageRequest.of(i, pageSize), false).toList();
+            List<Mouse> pageOfCreatedMouses = mouseController.getAll(PageRequest.of(i, pageSize,
+                    Sort.by("name").ascending()), false).toList();
 
-            List<String> expectedNames = listOfPages.get(i).stream()
+            List<Mouse> pageOfOriginalMouses = allOfMouses.subList(i * pageSize, (i * pageSize) + pageSize);
+            List<String> expectedNames = pageOfOriginalMouses.stream()
                     .map(Mouse::getName)
                     .toList();
             List<String> actualNames = pageOfCreatedMouses.stream()
